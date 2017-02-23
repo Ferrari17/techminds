3ea6a34b8967215cbb021636284701371b3aa3e5
darknight <narlarocks@gmail.com>
2017-02-23 17:48:07 -0600

    Adding Content

--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 
 # Just a test script
+Welcome to TechMinds
+
+This is a repository to host code
+used to mean a type of agile relationship between Development and IT Operations. 
+The goal of DevOps is to change and improve the relationship by advocating better communication and collaboration between the two business units.
