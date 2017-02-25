6e94de4e4ca40b94607d289e09779cad4d124192
darknight <narlarocks@gmail.com>
2017-02-24 18:35:37 -0600

    updated test script

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ Welcome to TechMinds
 This is a repository to host code
 
 The goal of DevOps is to change and improve the relationship by advocating better communication and collaboration between two business units.
+
+echo `Welcome to Techminds`
