f65e06c2bbc392710d84eedc9907c798309ef880
Vijay <devopsworks99@gmail.com>
2017-03-12 18:09:22 -0500

    removing test

--- test.sh
+++
@@ -1,9 +0,0 @@
-
-# Just a test script
-Welcome to TechMinds
-
-This is a repository to host code
-
-The goal of DevOps is to change and improve the relationship by advocating better communication and collaboration between two business units.
-
-echo `Welcome to Techminds`
